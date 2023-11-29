eda2ca74a67b301b498e1e876a6793eb788f1638
Ali Chraghi <alichraghi@proton.me>
2023-11-29 19:33:51 +0330

    update.sh: pinned clone

--- update.sh
+++ update.sh
@@ -1,7 +1,23 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-git clone https://github.com/KhronosGroup/SPIRV-Tools upstream --depth 1 || true
+# `git clone --depth 1` but at a specific revision
+git_clone_rev() {
+    repo=$1
+    rev=$2
+    dir=$3
+
+    rm -rf "$dir"
+    mkdir "$dir"
+    pushd "$dir"
+    git init -q
+    git fetch "$repo" "$rev" --depth 1
+    git checkout -q FETCH_HEAD
+    popd
+}
+
+SPIRV_TOOLS_REV=f4a73dd7a0cadfa9a9ea384b609e0e6a2cb71f5b
+git_clone_rev https://github.com/KhronosGroup/SPIRV-Tools $SPIRV_TOOLS_REV upstream
 pushd upstream
 python3 utils/git-sync-deps
 mkdir -p build && pushd build
@@ -9,6 +25,8 @@ cmake ..
 # enough to make sure headers are generated
 timeout 20 make || true
 popd
+rm -rf include-generated
+mkdir -p include-generated
 find build -name "*.inc" -exec cp -r '{}' ../include-generated/ \;
 popd
 rm -rf upstream
